6709a764188cfa549e55f7057035f9576a89f12b
Duyile Tolulope <andeladeveloper@Andelas-MacBook-Pro.local>
2017-06-14 00:23:41 +0100

    fix: root directory

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const path = require('path');
 
 const server = express();
-server.use(express.static(path.join(__dirname, '/')));
+server.use(express.static(path.join(__dirname, '/src')));
 server.listen(process.env.PORT || 8080);
